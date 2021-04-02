f62ca2d5bdee4c495668b44ecee2dd96f42f06c9
Ashesh Goplani <ashesh.goplani96@gmail.com>
2021-04-02 17:54:42 +0500

    Testing markdown in comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Innotrade enapso GraphDB Admin
 // (C) Copyright 2019-2020 Innotrade GmbH, Herzogenrath, NRW, Germany
 // Author: Alexander Schulze
-
+require('@innotrade/enapso-config');
 const EnapsoGraphDBAdmin = require('./lib/enapso-graphdb-admin');
 
 module.exports = {
